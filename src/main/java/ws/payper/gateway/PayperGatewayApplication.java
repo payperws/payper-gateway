944b86b6e7aed90363b86874bd85a9ae055d6e88
Alex Males <alexandru.males@gmail.com>
2018-10-27 06:29:23 +0300

    dynamic load of configured routes at start up

--- src/main/java/ws/payper/gateway/PayperGatewayApplication.java
+++ src/main/java/ws/payper/gateway/PayperGatewayApplication.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.server.ServerWebExchange;
+import ws.payper.gateway.config.Api;
+import ws.payper.gateway.config.Route;
 import ws.payper.gateway.config.RoutePriceConfiguration;
 
 @SpringBootApplication
@@ -25,16 +27,31 @@ public class PayperGatewayApplication {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("r1", r -> r.predicate(this::paymentReceiptMissing)
-                        .filters(f -> f.redirect(302, "http://localhost:8080/payment-required"))
-                        .uri("http://swapi.co/"))
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        config.getApis().forEach(api -> build(api, routes));
+        return routes.build();
+    }
 
-                .route("r2", r -> r.predicate(this::paymentReceiptValid)
-                        .filters(f -> f.removeRequestHeader(RECEIPT_HEADER))
-                        .uri("http://swapi.co/"))
+    private void build(Api api, RouteLocatorBuilder.Builder builder) {
+        api.getRoutes().forEach(route -> build(api, route, builder));
+    }
 
-                .build();
+    private void build(Api api, Route route, RouteLocatorBuilder.Builder builder) {
+        String routeId = api.getName() + "-" + route.getRoute();
+        String redirectUrl = getPaymentRequiredUrl();
+        builder
+                .route(routeId + "-r1",
+                        r -> r.predicate(this::paymentReceiptMissing)
+                                .filters(f -> f.redirect(302, redirectUrl))
+                                .uri(api.getBaseUrl()))
+                .route(routeId +"-r2",
+                        r -> r.predicate(this::paymentReceiptValid)
+                                .filters(f -> f.removeRequestHeader(RECEIPT_HEADER))
+                                .uri(api.getBaseUrl()));
+    }
+
+    private String getPaymentRequiredUrl() {
+        return "http://localhost:8080/payment-required";
     }
 
     private boolean paymentReceiptValid(ServerWebExchange serverWebExchange) {
